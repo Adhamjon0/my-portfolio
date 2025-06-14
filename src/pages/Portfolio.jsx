eee2cdca11a8ef3bcb228617bc05d9c7b1956f01
Adhamjon0 <adhamjonsodiqov03@gmail.com>
2025-06-14 13:13:18 +0500

    my-portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Portfolio.css'
-
+import uzumMarket from '../pages/photos/uzum.jpg'
 export default function Portfolio() {
     return (
         <div>
             <div className='portfolio-container'>
-                <h1>My Portfolio</h1>
+
+                <div className='uzum-market'>
+                    <h1>Projects</h1>
+                    <h2>Uzum market</h2>
+                    <img src={uzumMarket} className='uzum_img' alt="Uzum market" />
+                    <a href=" https://uzum-market-psi.vercel.app/" target="_blank">Uzum market</a>
+                </div>
+
+                <div className='kunuz'>
+
+                </div>
 
             </div>
         </div>
